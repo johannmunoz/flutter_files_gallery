cd054e77b3376fa63f9f9dd25c2492d503a923e5
Johann Munoz <johannmunoz.dev@gmail.com>
2020-03-27 16:13:26 +1030

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:files_gallery/files_gallery.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<String> urls = [
+    'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/baboon.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/peppers.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/goldhill.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/mountain.png',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Gallery'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            color: Colors.blue,
+            child: Gallery.network(urls),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/files_gallery.dart
@@ -0,0 +1,56 @@
+library files_gallery;
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class Gallery extends StatelessWidget {
+  final List<ImageProvider> images;
+  final VoidCallback onDeleteImage;
+
+  Gallery.file(List<File> files, {Key key, this.onDeleteImage})
+      : images = files.map((file) => FileImage(file)).toList(),
+        super(key: key);
+
+  Gallery.network(List<String> urls, {Key key, this.onDeleteImage})
+      : images = urls.map((url) => NetworkImage(url)).toList(),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, boxConstraints) {
+        var borderSize = 100.0;
+        if (MediaQuery.of(context).orientation == Orientation.landscape) {
+          borderSize = (boxConstraints.maxWidth / 6) - 2;
+        } else {
+          borderSize = (boxConstraints.maxWidth / 4) - 2;
+        }
+
+        return Row(
+          children: <Widget>[
+            Flexible(
+              fit: FlexFit.tight,
+              child: Wrap(
+                direction: Axis.horizontal,
+                spacing: 2.0,
+                children: images
+                    .map(
+                      (image) => Container(
+                        child: Image(
+                          image: image,
+                          height: borderSize,
+                          width: borderSize,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
