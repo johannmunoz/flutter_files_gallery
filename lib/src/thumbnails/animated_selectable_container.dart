eb3145068706344d997f029ac54856e84d032e5e
Johann Munoz <johannmunoz.dev@gmail.com>
2020-04-02 17:42:24 +1030

    Added animated container

---
+++ lib/src/thumbnails/animated_selectable_container.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class AnimatedSelectableContainer extends StatefulWidget {
+  final Widget child;
+  final VoidCallback onTap;
+  const AnimatedSelectableContainer({
+    Key key,
+    this.child,
+    this.onTap,
+  }) : super(key: key);
+  @override
+  _SelectableContainerState createState() => _SelectableContainerState();
+}
+
+class _SelectableContainerState extends State<AnimatedSelectableContainer> {
+  bool isSelected = true;
+  @override
+  Widget build(BuildContext context) {
+    return _imageContainer();
+  }
+
+  Widget _imageContainer() {
+    return LayoutBuilder(
+      builder: (context, boxConstraints) {
+        final sideWidth = boxConstraints.maxWidth;
+        return GestureDetector(
+          onTap: () {
+            setState(() => isSelected = !isSelected);
+            widget.onTap();
+          },
+          child: Container(
+            alignment: Alignment.center,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Container(
+                    child: Icon(
+                      Icons.image,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 150),
+                    height: isSelected ? sideWidth - 10 : sideWidth,
+                    width: isSelected ? sideWidth - 10 : sideWidth,
+                    child: widget.child,
+                  ),
+                ),
+                if (isSelected)
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 150),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: isSelected ? Colors.white : Colors.transparent,
+                      ),
+                      child: Icon(
+                        Icons.check_circle,
+                        color: Colors.blue[700],
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/src/thumbnails/selectable_container.dart
+++ lib/src/thumbnails/selectable_container.dart
@@ -1,3 +1,4 @@
+import 'package:files_gallery/src/thumbnails/placeholder_container.dart';
 import 'package:flutter/material.dart';
 
 class SelectableContainer extends StatefulWidget {
@@ -20,55 +21,41 @@ class _SelectableContainerState extends State<SelectableContainer> {
   }
 
   Widget _imageContainer() {
-    return LayoutBuilder(
-      builder: (context, boxConstraints) {
-        final sideWidth = boxConstraints.maxWidth;
-        return GestureDetector(
-          onTap: () {
-            setState(() => isSelected = !isSelected);
-            widget.onTap();
-          },
-          child: Container(
-            alignment: Alignment.center,
-            child: Stack(
-              children: [
-                Positioned.fill(
-                  child: Container(
-                    child: Icon(
-                      Icons.image,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 150),
-                    height: isSelected ? sideWidth - 10 : sideWidth,
-                    width: isSelected ? sideWidth - 10 : sideWidth,
-                    child: widget.child,
-                  ),
-                ),
-                if (isSelected)
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 150),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: isSelected ? Colors.white : Colors.transparent,
-                      ),
-                      child: Icon(
-                        Icons.check_circle,
-                        color: Colors.blue[700],
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        );
+    return GestureDetector(
+      onTap: () {
+        setState(() => isSelected = !isSelected);
+        widget.onTap();
       },
+      child: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Container(
+                child: Icon(
+                  Icons.image,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            PlaceholderContainer(child: widget.child),
+            if (isSelected)
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isSelected ? Colors.white : Colors.transparent,
+                  ),
+                  child: Icon(
+                    Icons.check_circle,
+                    color: Colors.blue[700],
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
