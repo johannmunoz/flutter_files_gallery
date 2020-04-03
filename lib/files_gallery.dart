a08569fdf8eb6c0de48e9495b2ba9171d8dba467
Johann Munoz <johannmunoz.dev@gmail.com>
2020-04-03 17:47:58 +1030

    Added show gallery

--- lib/files_gallery.dart
+++ lib/files_gallery.dart
@@ -1,5 +1,6 @@
 library files_gallery;
 
-export 'src/gallery.dart';
+// export 'src/gallery.dart';
+export 'src/show_gallery.dart';
 export 'src/selectable_gallery.dart';
 export 'src/file_types.dart';
